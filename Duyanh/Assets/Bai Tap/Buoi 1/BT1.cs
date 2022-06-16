8af7a76c58ffbcd7a2c6189a8b0d92272e80eb4b
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-06-16 15:05:39 +0700

    HJ

--- Duyanh/Assets/Bai Tap/Buoi 1/BT1.cs
+++ Duyanh/Assets/Bai Tap/Buoi 1/BT1.cs
@@ -7,7 +7,14 @@ public class BT1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // tính trung bình 4 số
+        int a = 8;
+        float b = 7.5f;
+        int c = 9;
+        float d = 8.5f;
+        float tbc = (a + b + c + d) / 4;
+        Debug.Log("TRUNG BINH CONG = " + tbc);
+
     }
 
     // Update is called once per frame
